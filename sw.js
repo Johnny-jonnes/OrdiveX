50418406f6eaa391306b79022a59adbec7e81c22
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-07-04 16:04:14 +0000

    bump: SW cache v9.7.47b (hotfix refresh)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v9.7.47';
+const CACHE_NAME = 'pharma-cache-v9.7.47b';
 const ASSETS = [
   './',
   './index.html',
