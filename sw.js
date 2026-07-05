29b0ab38be55d6a3af8061945df8d8ec8384e18c
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-07-05 12:24:25 +0000

    v9.7.47p: Force rebuild with updated cache versions and sw cache parameters

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v9.7.47n';
+const CACHE_NAME = 'pharma-cache-v9.7.47p';
 const ASSETS = [
   './',
   './index.html',
