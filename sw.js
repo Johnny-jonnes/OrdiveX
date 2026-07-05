4b51534efae16b8b64e663f52b59d5aa270e5711
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-07-05 12:28:59 +0000

    v9.7.47q: Add .nojekyll to fix static deployment and force Pages clean rebuild

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v9.7.47p';
+const CACHE_NAME = 'pharma-cache-v9.7.47q';
 const ASSETS = [
   './',
   './index.html',
