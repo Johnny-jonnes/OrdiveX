5225c2d49b6e6e47a9d4f4bbca56b11d20e415e4
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-07-05 10:36:22 +0000

    v9.7.47i: Trigger rebuild/redeploy and bump sw cache to v9.7.47i

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v9.7.47h';
+const CACHE_NAME = 'pharma-cache-v9.7.47i';
 const ASSETS = [
   './',
   './index.html',
