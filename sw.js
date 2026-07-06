86f99c5b8cf60967c922147e99d81163d2664482
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-07-06 12:23:46 +0000

    v9.7.48f: redeploy and cache-bust changes for network manager regression fixes

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v9.7.48e';
+const CACHE_NAME = 'pharma-cache-v9.7.48f';
 const ASSETS = [
   './',
   './index.html',
