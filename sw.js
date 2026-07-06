5c93c50c2dab3ae745e38fd712b0c859e60b5cef
Johnny <alsenycamaraditcamaraalseny@gmail.com>
2026-07-06 11:04:43 +0000

    v9.7.48b: Bump version for cache-busting and deployment

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache-first PWA strategy pour fonctionnement 100% offline
  */
 
-const CACHE_NAME = 'pharma-cache-v9.7.48a';
+const CACHE_NAME = 'pharma-cache-v9.7.48b';
 const ASSETS = [
   './',
   './index.html',
